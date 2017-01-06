134307e27f295194ff611e7dfa93c7dd2b5aaf53
Kevin Choi <kjchoi10@Kevins-MacBook-Pro.local>
2017-01-06 15:02:53 -0500

    finished first draft

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ app.apiUrl = 'https://api.spotify.com/v1';
 app.events = function () {
 	$('form').on('submit', function (e) {
 		e.preventDefault();
+		$('.loader').toggleClass('show');
 		let artists = $('input[type=search]').val();
 		artists = artists.split(',');
 		let search = artists.map(artistName => app.searchArtist(artistName));
-		
+
 		app.retreiveArtistInfo(search);
 		});
 };
@@ -37,9 +38,33 @@ app.getArtistAlbums = (artistId) => $.ajax({
 });
 
 
-// THen get tracks
-app.getArtistTracks = function() {
+// Then get tracks
+app.getArtistTracks = (id) => $.ajax({
+	url: 'https://api.spotify.com/v1/albums/' + id + '/tracks',
+	method: 'GET',
+	dataType: 'json',
+});
 
+// build playlist
+app.buildPlayList = function(tracks) {
+	$.when(...tracks)
+		.then((...tracksResults) => {
+			tracksResults = tracksResults.map(getFirstElement)
+			  .map(item => item.items)
+				.reduce(flatten, [])
+				.map(item => item.id);
+			const randomTracks = [];
+			for(let i = 0; i < 30; i++){
+				randomTracks.push(getRandomTrack(tracksResults));
+			}
+			const baseUrl = 'https://embed.spotify.com/?theme=white&uri=spotify:trackset:My Playlist:'
+			+ randomTracks.join();
+
+			$('.loader').toggleClass('show');
+			
+			$('.playlist').html('<iframe src="' + baseUrl + '"height="400"' + '></iframe>' );
+			console.log(randomTracks);
+		});
 };
 
 app.retreiveArtistInfo = function(search) {
@@ -49,22 +74,33 @@ app.retreiveArtistInfo = function(search) {
 				.map(res => res.artists.items[0].id) // gets album id
 			 	.map(id => app.getArtistAlbums(id));
 
-				app.retreiveArtistTracks(results);
+			app.retreiveArtistTracks(results);
 	});
 };
 
 app.retreiveArtistTracks = function(artistAlbums) {
 	$.when(...artistAlbums)
 		.then((...albums) => {
-			albums = albums.map(getFirstElement);
+			albumIds = albums.map(getFirstElement)
 				.map(res => res.items)
 				.reduce((prev, curr) => [...prev, ...curr], [])
-			console.log(albums);
+				.map(album => album.id)
+				.map(ids => app.getArtistTracks(ids));
+			app.buildPlayList(albumIds);
 		});
 };
 
 // gets the first item in the array to retrieveArtist Info
-const getFirstElement = {item} => item[0];
+const getFirstElement = (item) => item[0];
+
+// flatten function
+const flatten = (prev, curr) => [...prev, ...curr];
+
+// build random playlist track
+const getRandomTrack = (trackArray) => {
+	const randoNum = Math.floor(Math.random() * trackArray.length);
+	return trackArray[randoNum];
+};
 
 //Then build playlist
 
